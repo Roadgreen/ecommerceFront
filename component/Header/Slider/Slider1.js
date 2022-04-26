b6cbbbed428c49a50fa320de48ea9e67805b1205
Melvin.B <41331833+Roadgreen@users.noreply.github.com>
2022-04-26 18:08:54 +0200

    main

--- component/Header/Slider/Slider1.js
+++ component/Header/Slider/Slider1.js
@@ -1,11 +1,21 @@
-import { Fragment } from 'react'
+import { Fragment, useEffect, useState } from 'react'
 import styles from './Slider.module.css'
+import Image from 'next/image'
+import GifTea from '../../../public/img/Slider/SliderLoopThe/giftea.gif'
 
 export default function Slider(){
 
+      
+          
+    
+
+    
     return(
 <Fragment>
-    
+    <div className={styles.Slider}>
+<Image src={GifTea} className={styles.SliderImg} alt='Tea pot' width={350} height={500}/>
+<h1 className={styles.SliderH1}>Ici, Nous parlons thé.</h1>
+</div>
 </Fragment>
     )
 }
