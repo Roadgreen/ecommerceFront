dcb589235670d169ce0af2d435c3c999a7058481
Melvin.B <41331833+Roadgreen@users.noreply.github.com>
2022-05-18 21:04:18 +0200

    main

--- component/Card/HomeCard/CardBestProduct.js
+++ component/Card/HomeCard/CardBestProduct.js
@@ -3,6 +3,7 @@ import styles from './CardBestProduct.module.css'
 import Image from 'next/image'
 import Link from 'next/link'
 
+
 export default function Card() {
 
   const data = [{photos:'/img/productCard/BestProductCard/teapng.png',id:'pjpeioj',name:'Thé Pu Erh Moudjaidin',type:'thé noir de boula',price:{vingt:45,cinquante:70,cent:90 },note:5,description:'Thé Pu erh venue des montagne de losc en chine.'},{photos:'/img/productCard/BestProductCard/teapng.png',id:'pjpeioj',name:'Thé Pu Erh Moudjaidin',type:'thé noir de boula',price:{vingt:45,cinquante:70,cent:90 },note:5,description:'Thé Pu erh venue des montagne de losc en chine.'},{photos:'/img/productCard/BestProductCard/teapng.png',id:'pjpeioj',name:'Thé Pu Erh Moudjaidin',type:'thé noir de boula',price:{vingt:45,cinquante:70,cent:90 },note: 5,description:'Thé Pu erh venue des montagne de losc en chine.'}]
@@ -15,9 +16,9 @@ export default function Card() {
    
 
     return (
-      <div className={styles.Card} key={i}>
+      <Element activeClass={styles.Card}  className={styles.Card} key={i}>
         <div className={styles.img}>
-        <Image  src={item.photos} width={100} height={90}/>r
+        <Image  src={item.photos} width={100} height={85}/>
         </div>
        
         <h1>{item.name}</h1>
@@ -33,7 +34,7 @@ export default function Card() {
   <Link href={`/product/${encodeURIComponent(item.id)}`}>
   <button className={styles.rapid}>Découverte</button>
   </Link>
-      </div>
+      </Element>
     )
       
       
@@ -44,7 +45,7 @@ export default function Card() {
 
   return (
     <Fragment>
-      <h1 className={styles.h1}>Product</h1>
+      <h1 className={styles.Title}>Nos Bestseller</h1>
       <div className={styles.containerMapping}>{data.map(mapping)}</div>
       
       </Fragment>
