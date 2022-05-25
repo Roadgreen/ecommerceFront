2c438f160b49c8f40ee60108e71eea62fea32af5
Melvin.B <41331833+Roadgreen@users.noreply.github.com>
2022-05-25 20:10:59 +0200

    NEw blog

--- component/Body/Home/Home-Blog/HomeBlog.js
+++ component/Body/Home/Home-Blog/HomeBlog.js
@@ -1,17 +1,28 @@
 import React from 'react'
 import styles from './HomeBlog.module.css'
+import Image from 'next/image'
 
 
 
 export default function HomeBlog() {
-const data = [{Title:`Le thés et l'eau`,description:`Comment le thés et l'eau est devenue un breuvage`,img:'/img/Blog/HomeBlog/Blog1.jpg'}]
+const data = [{title:`Le thés et l'eau`,description:`Comment le thés et l'eau est devenue un breuvage`,img:'/img/Blog/HomeBlog/Blog1.jpg'},{title:`Le thés et l'eau`,description:`Comment le thés et l'eau est devenue un breuvage`,img:'/img/Blog/HomeBlog/Blog1.jpg'},{title:`Le thés et l'eau`,description:`Comment le thés et l'eau est devenue un breuvage`,img:'/img/Blog/HomeBlog/Blog1.jpg'}]
 
+const mapping = (item,i) => {
+return(
+  <div className={styles.Card} key={i}>
+    <Image className={styles.img} src={item.img} width={366} height={202}/>
+    <h1 className={styles.h1}>{item.title}</h1>
+    <h3 className={styles.h3}>{item.description}</h3>
+ 
+  </div>
+)
+}
 
 
   return (
 
     <div className={styles.container}>
-
+        {data.map(mapping)}
     </div>
   )
 }
