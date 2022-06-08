44e87547412530f6ab7e2ffa8c1ab0625160c684
Melvin.B <41331833+Roadgreen@users.noreply.github.com>
2022-06-08 18:53:20 +0200

    master

--- component/Body/Home/Footer/FooterUn/Footer.js
+++ component/Body/Home/Footer/FooterUn/Footer.js
@@ -1,18 +1,39 @@
 import React from 'react'
 import styles from './Footer.module.css'
-
+import Link from 'next/link'
+import Image from 'next/image'
+import logo from '../../../../../public/img/LogoMenu/logoDesktop.svg'
+import insta from '../../../../../public/img/picto/PictoFooter/insta.png'
+import facebook from '../../../../../public/img/picto/PictoFooter/facebook.png'
+import tik from '../../../../../public/img/picto/PictoFooter/tik.png'
 
 export function Footer() {
   return (
     
     <div className={styles.container}>
-      <div className={styles.containerEtea}> <h2>Etea</h2>
-      <li>Qui sommes nous?</li>
-      <li>Notre Engagement</li>
-      <li>Aide-FAQ</li>
-      <li>Conditions Générales de Ventes</li>
-      <li>Livraison</li>
-      <li>Mentions Légales</li></div>
+      <div className={styles.containerEtea}> 
+      <li><Link href={'/'}>Qui sommes nous?</Link></li>
+      <li><Link href={'/'}>Notre Engagement</Link></li>
+      <li><Link href={'/'}>Aide-FAQ</Link></li>
+      <li><Link href={'/'}>Conditions Générales de Ventes</Link></li>
+      <li><Link href={'/'}>Livraison</Link></li>
+      <li><Link href={'/'}>Mentions Légales</Link></li>
+     </div>
+     <div className={styles.logo}>
+     <Image  src={logo} width={90} height={90}/>
+     <p>©2022 ETEA </p>
+     </div>
+     <div className={styles.containerSocial}>
+       <h3>Suivez-nous : </h3>
+       <div><Link href={'/'}><Image className={styles.Image} layout='fixed' src={facebook} width={30}
+       height={30} /></Link>
+       <Link href={'/'}><Image layout='fixed' className={styles.Image} src={insta} width={30}
+       height={30} /></Link>
+       <Link href={'/'}><Image layout='fixed' className={styles.Image} src={tik} width={30}
+       height={30} /></Link></div>
+       
+      
+        </div>
      
     </div>
   )
