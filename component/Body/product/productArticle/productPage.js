9c3f96665819c2fb0f1203d976433957333eef41
Melvin.B <41331833+Roadgreen@users.noreply.github.com>
2022-06-15 00:11:21 +0200

    master

--- component/Body/product/productArticle/productPage.js
+++ component/Body/product/productArticle/productPage.js
@@ -1,7 +1,17 @@
 import React from 'react'
+import styles from './productPage.module.css'
+import Image from 'next/image'
 
-export default function productPage() {
+export default function productPage(props) {
+  const data = props.data[0];
+  console.log(data)
   return (
-    <div>productPage</div>
+    <div className={styles.container}>
+      <div className={styles.photos}>
+        <button></button>
+        <Image className={styles.img} src={data.photos} layout="fixed" width={200} height={200} />
+        <button></button>
+      </div>
+    </div>
   )
 }
--- pages/product/[article].js
+++ pages/product/[article].js
@@ -1,8 +1,13 @@
-
+import Productpage from "../../component/Body/product/productArticle/productPage"
 
 export default function article() {
+  
+  let data = [{photos:'/img/productCard/BestProductCard/teapng.png',
+    edition:'Best-seller',id:'pjpeioj',name:'Thé Pu Erh Moudjaidin',type:'thé noir de boula',price:{vingt:45,cinquante:70,cent:90 },note:5,description:'Thé Pu erh venue des montagne de losc en chine.'}]
+
   return (
     <>
+    <Productpage data={data}/>
     </>
   )
 }
